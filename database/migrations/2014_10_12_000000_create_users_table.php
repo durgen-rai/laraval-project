e70e8670509ece0f28276ace485e3d793e1a184c
durgen-rai <raidurgen@gmail.com>
2020-04-09 22:40:37 +0545

    added new migration file for user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('username',100)->unique();
+            $table->string('email',100)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password',100)->nullable();
+            $table->string('full_name',100);
+            $table->string('contact_n0',100);
+            $table->string('address',100);
+            $table->boolean('status',1)->default(0);
+
         });
     }
 
